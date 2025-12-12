d4cd23955119123d264fbfd625cda7cb65348864
ZachChll <Zacharie.Chaillou@etu.univ-smb.fr>
2025-12-12 11:41:07 +0100

    rajout du Threading pour le  DispatcherTimer

--- CrownSurvivor/UCJeu.xaml.cs
+++ CrownSurvivor/UCJeu.xaml.cs
@@ -13,16 +13,38 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Windows.Threading;
+//rajout 
+
 namespace CrownSurvivor
-{
+{   
     /// <summary>
     /// Logique d'interaction pour UCJeu.xaml
     /// </summary>
     public partial class UCJeu : UserControl
     {
+        bool goleft, goright, goup, godown;
+        int vitesse = 10;
+        int speed = 12;
+        
+        DispatcherTimer gameTimer = new DispatcherTimer();
         public UCJeu()
         {
             InitializeComponent();
+            Canvas.SetLeft(Perso, 100);
+            Canvas.SetTop(Perso, 100);
+            Perso.Focusable = true;
+            this.Focus();
+        }
+
+        private void KeysIsDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+
         }
     }
 }
